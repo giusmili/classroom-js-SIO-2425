adc57a03a6d1342c8a4ece1ba1a5933d46c1d7e3
giusmili <giusmili67@gmail.com>
2024-12-09 17:53:33 +0100

    js

---
+++ js/demo.js
@@ -0,0 +1,22 @@
+(() => {
+  /*
+  =================
+  fonction anonyme
+  =================
+  */
+})();
+/* notre code ici */
+let x = Number(10),
+    y = String("10");
+let total = x + y.length;
+const code = parseInt(3.5);
+
+const langs = ["fr", "it", "en"];
+console.table(langs.length);
+
+x === y ? console.log(true) : console.error(false);
+
+/* Objet.méthode.propriété : values */
+console.log(code); /* résultat de la variable x */
+
+console.log(" Résultat " + total);
