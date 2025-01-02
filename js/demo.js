1415c10fdc04fa372e50fd0dd146a958e57ee416
giusmili <giusmili67@gmail.com>
2025-01-02 18:09:51 +0100

    js

--- js/demo.js
+++ js/demo.js
@@ -18,5 +18,20 @@ x === y ? console.log(true) : console.error(false);
 
 /* Objet.méthode.propriété : values */
 console.log(code); /* résultat de la variable x */
-
 console.log(" Résultat " + total);
+const lang = navigator.language;
+console.log(lang);
+
+/* OBJECT JS */
+
+const User = {
+  name: "Marrin",
+  last_name: "Bob",
+  age: null
+
+};
+
+Object.seal(User);
+User.ville = "Paris"; /* empeche d'ajouter une nouvelle propriété */
+delete Object.name; /* cela empeche de supprimer une propriété */
+console.table(User);
