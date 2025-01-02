6265f3ff334479c02254f0a8a47b522e777c4c29
giusmili <giusmili67@gmail.com>
2025-01-02 18:12:04 +0100

    comment

--- js/demo.js
+++ js/demo.js
@@ -4,35 +4,35 @@
   fonction anonyme
   =================
   */
+  /* notre code ici */
+  let x = Number(10),
+      y = String("10");
+  let total = x + y.length;
+  const code = parseInt(3.5);
+
+  const langs = ["fr", "it", "en"];
+  console.table(langs.length);
+
+  x === y ? console.log(true) : console.error(false);
+
+  /* Objet.méthode.propriété : values */
+  console.log(code); /* résultat de la variable x */
+  console.log(" Résultat " + total);
+  const lang = navigator.language;
+  console.log(lang);
+
+  /* OBJECT JS */
+
+  const User = {
+    name: "Marrin",
+    last_name: "Bob",
+    age: null
+
+  };
+
+  Object.seal(User); // voir Object.freeze()
+  User.ville = "Paris"; /* empeche d'ajouter une nouvelle propriété */
+
+  delete Object.name; /* cela empeche de supprimer une propriété */
+  console.table(User);
 })();
-/* notre code ici */
-let x = Number(10),
-    y = String("10");
-let total = x + y.length;
-const code = parseInt(3.5);
-
-const langs = ["fr", "it", "en"];
-console.table(langs.length);
-
-x === y ? console.log(true) : console.error(false);
-
-/* Objet.méthode.propriété : values */
-console.log(code); /* résultat de la variable x */
-console.log(" Résultat " + total);
-const lang = navigator.language;
-console.log(lang);
-
-/* OBJECT JS */
-
-const User = {
-  name: "Marrin",
-  last_name: "Bob",
-  age: null
-
-};
-
-Object.seal(User);
-User.ville = "Paris"; /* empeche d'ajouter une nouvelle propriété */
-
-delete Object.name; /* cela empeche de supprimer une propriété */
-console.table(User);
