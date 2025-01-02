9139b510707781285a839162b6754123387d781d
giusmili <giusmili67@gmail.com>
2025-01-02 17:42:20 +0100

    js

--- js/app.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-
-    const userLang = navigator.language || navigator.userLanguage; // Langue principale du client
-    console.info(`Langue détectée : ${userLang}`);
-    /*   document.getElementById("lang").textContent += ` Client détectée : ${userLang}`; */
-});
